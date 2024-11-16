0bd1f91946e2c25c28389349bdce58ae70bc7555
Patas444 <lucio.robles.gaspar@gmail.com>
2024-11-16 16:16:36 -0600

    The Main Menu is be added
    A functional Main menu be added

---
+++ VR_LSS/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void SceneChanger(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
