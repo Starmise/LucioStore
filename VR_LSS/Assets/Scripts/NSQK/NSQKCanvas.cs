73443963e7da24e33f091ba1e59d2fce6deb1014
Luis R <78448904+Starmise@users.noreply.github.com>
2024-12-06 14:19:36 -0600

    Mainmenu and NavMesh

---
+++ VR_LSS/Assets/Scripts/NSQK/NSQKCanvas.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NSQKCanvas : MonoBehaviour
+{
+    public InputActionAsset inputActions;
+
+    private Canvas canvas;
+    private InputAction menu;
+
+    private void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        //menu = inputActions.FindActionMap("XRI LeftHand").FindAction("Menu");
+        //menu.Enable();
+        //menu.performed += OpenMenu;
+    }
+
+    private void OnDestroy()
+    {
+        menu.performed -= OpenMenu;
+    }
+
+    public void OpenMenu(InputAction.CallbackContext context)
+    {
+        canvas.enabled = !canvas.enabled;
+    }
+}
---
+++ VR_LSS/Assets/Scripts/NSQK/SavePlayerName.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SavePlayerName : MonoBehaviour
+{
+    public TMP_InputField nameInputField;
+    public TMP_Text nameText;
+
+    public void SaveName()
+    {
+        string playerName = nameInputField.text;
+        PlayerPrefs.SetString("PlayerName", playerName);
+        PlayerPrefs.Save();
+
+        nameText.gameObject.SetActive(true);
+        nameText.text = $"Oh Shhh, here we go again... {playerName}";
+
+        Debug.Log("Nombre guardado: " + playerName);
+    }
+}
